444706f5f0976ec9296ee998fdb5366c2ee2821c
Yash Aggarwal <yash@Yashs-MacBook-Pro.local>
2020-03-27 22:36:14 -0500

    add prettier, flesh things out

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,13 @@
 module.exports = {
     presets: [
         [
-            '@babel/preset-env',
+            "@babel/preset-env",
             {
                 targets: {
-                    node: 'current',
+                    node: "current",
                 },
             },
         ],
     ],
-    "plugins": [
-        "babel-plugin-goto"
-    ]
+    plugins: ["babel-plugin-goto"],
 };
--- challenges/1-grades/grades.test.js
+++ challenges/1-grades/grades.test.js
@@ -1,28 +1,27 @@
-import letterGrade from './grades';
+import letterGrade from "./grades";
 
-
-describe('grades', () => {
-    it.each([0, 25, 50, 59])('returns an F for %i', (num) => {
-        expect(letterGrade(num)).toEqual('F');
+describe("grades", () => {
+    it.each([0, 25, 50, 59])("returns an F for %i", (num) => {
+        expect(letterGrade(num)).toEqual("F");
     });
 
-    it.each([60, 69, 65, 62])('returns an D for %i', (num) => {
-        expect(letterGrade(num)).toEqual('D');
+    it.each([60, 69, 65, 62])("returns an D for %i", (num) => {
+        expect(letterGrade(num)).toEqual("D");
     });
 
-    it.each([70, 75, 71, 79])('returns an C for %i', (num) => {
-        expect(letterGrade(num)).toEqual('C');
+    it.each([70, 75, 71, 79])("returns an C for %i", (num) => {
+        expect(letterGrade(num)).toEqual("C");
     });
 
-    it.each([80, 85, 81, 89])('returns an B for %i', (num) => {
-        expect(letterGrade(num)).toEqual('B');
+    it.each([80, 85, 81, 89])("returns an B for %i", (num) => {
+        expect(letterGrade(num)).toEqual("B");
     });
 
-    it.each([90, 93, 97, 99, 100])('returns an A for %i', (num) => {
-        expect(letterGrade(num)).toEqual('A');
+    it.each([90, 93, 97, 99, 100])("returns an A for %i", (num) => {
+        expect(letterGrade(num)).toEqual("A");
     });
 
-    it.each([-1, -5, 101, 999])('returns null for %i', (num) => {
+    it.each([-1, -5, 101, 999])("returns null for %i", (num) => {
         expect(letterGrade(num)).toBeNull();
     });
-})
+});
