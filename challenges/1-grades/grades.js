46c500b4d45d8fc52baaf437b72fa810ca359db6
Yash Aggarwal <yash@Yashs-MacBook-Pro.local>
2020-03-27 22:03:13 -0500

    initial set

---
+++ challenges/1-grades/grades.js
@@ -0,0 +1,19 @@
+/*
+    For this challenge, you must implement this function that takes in a Number as an argument called numberGrade.
+    You must return the correct letter grade associated with this number.
+
+    Rules (all numbers are inclusive):
+        0-59: F
+        60-69: D
+        70-79: C
+        80-89: B
+        90-100: A
+    
+    If the value is not between 0 and 100, return null.
+*/
+
+function letterGrade(numberGrade) {
+    return null;
+}
+
+export default letterGrade;
